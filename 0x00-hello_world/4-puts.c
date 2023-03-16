d899a48fbbfb5eb63ce0b792f4b0a5d899d6a4c7
Leeshay <shayharypursat@gmail.com>
2023-03-16 01:33:05 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * master -Entry - A program that prints a line with puts function
+ * main -Entry - A program that prints a line with puts function
  * Return: 0
  */
-int master(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
