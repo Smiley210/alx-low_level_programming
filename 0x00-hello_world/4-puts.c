d82614e4af11cd363f08a297555016b00ae378a7
Leeshay <shayharypursat@gmail.com>
2023-03-16 11:22:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -Entry - A program that prints a line with puts function
+ * main -Entry - A program that prints a line with put function
  * Return: 0
  */
 int main(void)
